470b2a3a4397b3253423081e856fb542694aef23
hushuna <19945096907@136.com>
2019-10-17 14:07:42 +0800

    zuul

---
+++ src/main/java/com/zhiliao/ZhiliaoZuulApplication.java
@@ -0,0 +1,18 @@
+package com.zhiliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZhiliaoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhiliaoZuulApplication.class, args);
+	}
+
+}
