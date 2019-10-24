1708ac85a5313c9d5619804efec91a332996f2c6
24544 <2424856636@qq.com>
2019-10-24 18:52:33 +0800

    获取token

--- src/main/java/com/zhiliao/config/RedisConfig.java
+++ src/main/java/com/zhiliao/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.zhiliao.config;
 
-
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
