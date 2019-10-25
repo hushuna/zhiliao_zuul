96ea30b00d703213cfa5f31aea168f0585132e78
24544 <2424856636@qq.com>
2019-10-25 14:02:41 +0800

    redis工具类

--- src/main/java/com/zhiliao/controller/TestGetRedis.java
+++ src/main/java/com/zhiliao/controller/TestGetRedis.java
@@ -1,6 +1,7 @@
 package com.zhiliao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/test")
 public class TestGetRedis {
 
+    private static RedisTemplate<Object, Object> myRedisTemplate = null;
+
     @Autowired
-    private static RedisTemplate<Object, Object> myRedisTemplate;
+    public TestGetRedis(@Qualifier("myRedisTemplate") RedisTemplate<Object, Object> myRedisTemplate) {
+        TestGetRedis.myRedisTemplate = myRedisTemplate;
+    }
 
     @GetMapping(value = "/getKey")
-    public String getKey(){
-        String key="name";
-        String s = (String) myRedisTemplate.opsForValue().get(key);
+    public void getKey(){
+        String key="name2";
+        String s = String.valueOf(myRedisTemplate.opsForValue().get(key));
         System.out.println(s);
-        return s;
+        String s1 = String.valueOf(myRedisTemplate.opsForHash().get("name1", "snica"));
+        System.out.println(s1);
+    }
+
+    @GetMapping(value = "/put")
+    public void put(){
+        myRedisTemplate.opsForValue().set("name2","333");
     }
 }
--- src/main/java/com/zhiliao/utils/RedisUtil.java
+++ src/main/java/com/zhiliao/utils/RedisUtil.java
@@ -1,13 +1,19 @@
 package com.zhiliao.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisUtil {
+
+    private static RedisTemplate<Object, Object> myRedisTemplate = null;
+
     @Autowired
-    private static RedisTemplate<Object, Object> myRedisTemplate;
+    public RedisUtil(@Qualifier("myRedisTemplate") RedisTemplate<Object, Object> myRedisTemplate) {
+        RedisUtil.myRedisTemplate = myRedisTemplate;
+    }
 
     /**
      * 根据key获取缓存的值
@@ -16,4 +22,14 @@ public class RedisUtil {
     public static String get(String key){
         return (String) myRedisTemplate.opsForValue().get(key);
     }
+
+    /**
+     * 向缓存中存储数据
+     * @param key
+     * @param value
+     */
+    public static void setValue(String key,String value){
+        myRedisTemplate.opsForValue().set(key,value);
+    }
+
 }
