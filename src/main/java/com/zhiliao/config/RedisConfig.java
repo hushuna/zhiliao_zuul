284109b37aede59a5afd6e306759865088e92bbe
24544 <2424856636@qq.com>
2019-10-26 14:46:03 +0800

    x

--- src/main/java/com/zhiliao/config/RedisConfig.java
+++ src/main/java/com/zhiliao/config/RedisConfig.java
@@ -25,32 +25,4 @@ public class RedisConfig extends CachingConfigurerSupport {
         return template;
     }
 
-/*    public CacheManager cacheManager(RedisTemplate redisTemplate){
-
-        RedisCacheManager manager = new RedisCacheManager(redisTemplate);
-        return
-    }*/
-
-
-
-/*    *//**
-     * 缓存管理器
-     * @param redisConnectionFactory
-     * @return
-     *//*
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        //初始化一个RedisCacheWriter
-        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory);
-        //设置CacheManager的值序列化方式为json序列化
-        RedisSerializer<Object> jsonSerializer = new GenericJackson2JsonRedisSerializer();
-        RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair
-                .fromSerializer(jsonSerializer);
-        RedisCacheConfiguration defaultCacheConfig=RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(pair);
-        //设置默认超过期时间是30秒
-        defaultCacheConfig.entryTtl(Duration.ofSeconds(30));
-        //初始化RedisCacheManager
-        return new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
-    }*/
 }
