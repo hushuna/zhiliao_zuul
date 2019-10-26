2b9c8d1aae58d2d17491ffe9a7c970f716bc241e
24544 <2424856636@qq.com>
2019-10-26 17:01:05 +0800

    x

--- src/main/java/com/zhiliao/controller/AuthenticationController.java
+++ src/main/java/com/zhiliao/controller/AuthenticationController.java
@@ -1,12 +1,5 @@
 package com.zhiliao.controller;
 
-import com.zhiliao.comment.BizException;
-import com.zhiliao.comment.Result;
-import com.zhiliao.service.AuthenticationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,24 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/authentication")
 public class AuthenticationController {
 
-    @Autowired
-    private AuthenticationService authenticationService;
 
-    private Logger logger=LoggerFactory.getLogger(AuthenticationController.class);
-
-    /**
-     * 获取token
-     */
-    @PostMapping(value = "/getToken")
-    public Result getToken() {
-        try {
-            return Result.createSuccess("200",authenticationService.getToken());
-        }catch (BizException b){
-            logger.error("<<======获取token-失败 url:{{}} params:{}","/authentication/getToken",b);
-            return Result.createBizError(-500,b.getMessage());
-        }catch (Exception e) {
-            logger.error("<<======获取token-失败 url:{{}} params:{}","/authentication/getToken",e);
-            return Result.createError();
-        }
-    }
 }
--- src/main/java/com/zhiliao/service/AuthenticationService.java
+++ src/main/java/com/zhiliao/service/AuthenticationService.java
@@ -1,76 +1,8 @@
 package com.zhiliao.service;
 
-import com.zhiliao.comment.BizException;
-import com.zhiliao.utils.RedisUtil;
-import com.zhiliao.utils.RsaUtil;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class AuthenticationService {
-
-    /**
-     * 获取密钥
-     */
-    public void getPublicKey() throws Exception {
-        KeyPair keyPair = RsaUtil.getKeyPair();
-        String publicKey = RsaUtil.getPublicKey(keyPair);
-        String privateKey = RsaUtil.getPrivateKey(keyPair);
-        String publicKey1 = publicKey.replaceAll("\r\n", "");
-        String privateKey1 = privateKey.replaceAll("\r\n", "");
-        RedisUtil.setHash("key",publicKey1,privateKey1);
-    }
-
-    /**
-     * 获取token
-     */
-    public Map<String,Object> getToken() throws Exception {
-        boolean hasKey = RedisUtil.hasKey("key");
-        Integer i = (int) (Math.random() * 9 + 1) * 100000;
-        String text = String.valueOf(i);
-        Map<String,Object> map = new HashMap<>();
-        if (hasKey){
-            return generateToken(text, map);
-        }else {
-            getPublicKey();
-            return generateToken(text,map);
-        }
-    }
-
-    /**
-     * 生成token
-     * @param text
-     * @param map
-     * @return
-     * @throws Exception
-     */
-    private Map<String, Object> generateToken(String text, Map<String, Object> map) throws Exception {
-        Map<Object, Object> key = RedisUtil.getHash("key");
-        String publicKey="";
-        String privateKey="";
-        for (Object o : key.keySet()) {
-            publicKey = o.toString();
-            privateKey = key.get(o).toString();
-        }
-        byte[] bytes = text.getBytes();
-        String s = RsaUtil.byte2Base64(bytes);
-        byte[] bytes1 = RsaUtil.base642Byte(s);
-        PublicKey publicKey1 = RsaUtil.string2PublicKey(publicKey);
-        byte[] bytes2 = RsaUtil.publicEncrypt(bytes1, publicKey1);
-        String token = RsaUtil.byte2Base64(bytes2);
-        token = publicKey.replaceAll("\r\n", "");
-        token = privateKey.replaceAll("\n", "");
-        token = privateKey.replaceAll("\\+", "");
-        RedisUtil.setValue(token,token);
-        RedisUtil.setTime(token,token, (long) 3);
-        map.put("token",token);
-        return map;
-    }
 
 }
