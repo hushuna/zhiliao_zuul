39fade5eb66a2cc5544217a37db07aa29502bf54
hushuna <19945096907@136.com>
2019-12-23 09:04:42 +0800

    网关

--- src/main/java/com/zhiliao/ZhiliaoZuulApplication.java
+++ src/main/java/com/zhiliao/ZhiliaoZuulApplication.java
@@ -1,10 +1,12 @@
 package com.zhiliao;
 
+import com.zhiliao.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
@@ -14,5 +16,11 @@ public class ZhiliaoZuulApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ZhiliaoZuulApplication.class, args);
 	}
+	@Bean
+	public AccessFilter tokenFilter() {
+		return new AccessFilter();
+	}
 
 }
+
+
---
+++ src/main/java/com/zhiliao/filter/AccessFilter.java
@@ -0,0 +1,55 @@
+package com.zhiliao.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import io.micrometer.core.instrument.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class AccessFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(AccessFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        logger.info("--->>> TokenFilter {},{}", request.getMethod(), request.getRequestURL().toString());
+
+        String token = request.getParameter("token");// 获取请求的参数
+
+        if (StringUtils.isNotBlank(token)) {
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+
+    }
+}
